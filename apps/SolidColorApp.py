d7d8faa0f31592c92cd979c6b3683b35cfe0a08d
Michael Equi <michaelequi@berkeley.edu>
2022-01-15 19:58:22 -0800

    Added ability to set range of LEDS to light up

--- apps/SolidColorApp.py
+++ apps/SolidColorApp.py
@@ -5,13 +5,21 @@ from drlm_common.datatypes import Color
 
 
 class SolidColor(DrlmApp):
-    def __init__(self, color: Color, host: str = 'localhost', port: int = 5555):
+    def __init__(self, color: Color, ranges = [], host: str = 'localhost', port: int = 5555):
         super().__init__(host, port)
         self.color = color
+        self.ranges = ranges
 
     def run(self):
-        for i in range(self.num_leds):
-            self.strip.set_led(i, self.color)
+        if len(self.ranges) == 0:
+            for i in range(self.num_leds):
+                self.strip.set_led(i, self.color)
+        else:
+            for lower, upper in self.ranges:
+                assert lower > 0 and lower <= self.num_leds
+                assert upper >= lower and upper <= self.num_leds
+                for i in range(lower - 1, upper):
+                    self.strip.set_led(i, self.color)
         while True:
             self.write()
             time.sleep(10)
